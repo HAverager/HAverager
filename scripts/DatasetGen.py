c2e8a8473dd2efcbb341757af89733b1a9c0ede5
Mikhail Karnevskiy <mikhail.karnevskiy@desy.de>
2017-11-29 17:21:05 +0100

    Move script to scropts folder

---
+++ scripts/DatasetGen.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+import numpy as np
+from optparse import OptionParser
+
+# Dataset generator is a script to produce fake datasets in order to test averager
+
+########################
+# setup input parameters
+########################
+usage = ("DatasetGen.py parameters \nCreate dataset with gaussian uncertainties.  \n")
+parser = OptionParser(usage)
+parser.add_option("-d",dest="nData", default=50, type="int", help="Number of data points")
+parser.add_option("-m",dest="nMes", default=2, type="int", help="Number of measurements")
+parser.add_option("-s",dest="nSyst", default=10, type="int", help="Number of systematic sources")
+
+parser.add_option("-M", dest="zMes", default=0.99, type="float", help="Fraction of non-emply measurements")
+parser.add_option("-S", dest="zSyst", default=0.99, type="float", help="Fraction of non-emply systematics")
+
+options, arguments = parser.parse_args()
+
+# Number of data points, measurements, systematic
+nData = options.nData
+nMes = options.nMes
+nSyst = options.nSyst
+
+# part of non-emply measurement/systematics (between 0 and 1)
+zMes = options.zMes
+zSyst = options.zSyst
+
+# Min and mix number of measurements for data point (have to be smaller as total number of measurements) 
+#rMes = [1,3]
+
+# Min and mix number of systematic uncertainties for measurement (have to be smaller as total number of systematic uncertainties) 
+#rSyst = [3,4]
+
+
+# Parameters of uncertainties
+# Minumal and maximal relative statistical/systematic uncertainty
+vStat = [0.01,0.03]
+vSyst = [0.08,0.11]
+
+
+
+# array of truth data points
+Tdata = nData*np.random.random_sample((nData))+1
+
+# array of truth nuisanse parameters
+Tshift = np.random.normal(0, 1, nSyst)
+
+# array of statistical uncertainties. 
+# 2D data points vs measurements
+Mstat = (vStat[1]-vStat[0]) * np.random.random_sample((nMes, nData)) + vStat[0]
+
+# array of systematic uncertainties. 
+# 3D data points vs measurement vs systematic
+Msyst = (vSyst[1]-vSyst[0]) * np.random.random_sample((nMes, nData, nSyst)) + vSyst[0]
+
+# Add holes in the array of measurements and systematics
+# - Some data points does not exists for a certain measurement
+# - Some systematics does not exist for a certain measurement
+# --------------------------------
+Hdata = np.signbit(np.random.random_sample((nMes, nData))-zMes)*1
+Hsyst = np.signbit(np.random.random_sample((nMes, 1, nSyst))-zSyst)*1
+
+# array of measured data points
+Gaus_ij = np.random.normal(0, 1, nMes*nData).reshape((nMes, nData))
+Mdata = Tdata*(1+(Mstat*Gaus_ij)+np.sum(Msyst*Tshift*Hsyst,axis=2))*Hdata
+
+np.savetxt('Tshift.out', Tshift, fmt='%1.3f')
+np.savetxt('Tdata.out', Tdata, fmt='%1.3f')
+
+# Write output files. Python format
+# Loop over measurements
+for m in range(nMes):
+	f = open('test'+str(m)+'.csv','w')
+	f.write('bin1,data,stat')
+	# Loop over systematics
+	for s in range(nSyst):
+		if(Hsyst[m][0][s]!=0):
+			f.write(',error%05i' % s)
+	f.write('\n')
+	# Loop over data point
+	for d in range(nData):
+		if(np.abs(Mdata[m][d])>0.1):
+			f.write('%4.0f,'% d)
+			f.write('%8.3f,'% (Mdata[m][d]))
+			f.write('%8.3f'% (Mstat[m][d]*Mdata[m][d]))	
+			# Loop over systematics
+			for s in range(nSyst):
+				if(Hsyst[m][0][s]!=0):
+					f.write(',%8.3f'% (Msyst[m][d][s]*Mdata[m][d]))
+			f.write('\n')	
+	f.close()
+
+# Write output files. Fortran format
+# Loop over measurements
+for m in range(nMes):
+	f = open('test'+str(m)+'.dat','w')
+
+	f.write('&Data\n')
+	f.write('   Name = \'Data%i\'\n'%nData)
+	f.write('   NData = %i\n'% nData)
+	f.write('   NColumn = %i\n'%(nSyst+3))
+	f.write('   ColumnType = \'Bin\', \'Sigma\', %i*\'Error\'\n'%(nSyst+1))
+	f.write('   ColumnName = \'Y\', \'x-section\', \'stat\'')
+	for s in range(nSyst):
+		if(Msyst[m][0][s]!=0):
+			f.write(',\'error%05i\'' % s)
+	f.write('\n')
+	f.write('   Reaction = \'Bla\'\n')
+	f.write('   Percent = false')
+	for s in range(nSyst):
+		if(Hsyst[m][0][s]!=0):
+			f.write(',false')
+	f.write('\n')
+	f.write('&END\n')
+
+	# Loop over data point
+	for d in range(nData):
+		if(Mdata[m][d]!=0):
+			f.write('%4.0f '% d)
+			f.write('%8.3f '% Mdata[m][d])		
+			f.write('%8.3f '% (Mstat[m][d]*Mdata[m][d]))	
+			# Loop over systematics
+			for s in range(nSyst):
+				if(Hsyst[m][0][s]!=0):
+					f.write('%8.3f '% (Msyst[m][d][s]*Mdata[m][d]))
+			f.write('\n')
+	f.write('\n')
+	f.close()
