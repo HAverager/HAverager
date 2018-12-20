8b24e10d094bc6aaf558a4ee5654d51d6e0bd931
Mikhail Karnevskiy <mikhail.karnevskiy@desy.de>
2018-12-20 16:37:10 +0100

    Make files compatible with new python scripts

--- test/ZY/test.py
+++ test/ZY/test.py
@@ -1,49 +1,33 @@
 #!/usr/bin/env python
 
 # Import data reader and averager
-import averager
+import PyAverager
 import DataReader
 
 #read the data
 data,stat,syst = DataReader.paverage('','','')
 
-print stat
 stat = stat/100*data
 syst = syst/100*data
 
 # extract optional information
-snames=[x.ljust(32) for x in DataReader.oerror]
-fnames=[x.ljust(32) for x in DataReader.fnames]
-bins = [x.ljust(32) for x in DataReader.bins]
-binnames = DataReader.binnames
+snames=DataReader.oerror
+fnames=DataReader.fnames
 
-#print input information
-print 'Print Binning'
-print DataReader.binnames
+bins = DataReader.binnames
+binnames = DataReader.bins
 
-print 'Print Data'
-print data
+# Set input parameters
+PyAverager.OutFolder = './TOutP'
+PyAverager.nIterations = 0
+PyAverager.writeoriginal = True
+PyAverager.fixstat = False
+PyAverager.correctstatbias = False
+PyAverager.rescalestatsep = False
+PyAverager.postrotatesyst = False
+PyAverager.dosystimpact = False
+PyAverager.ntoymc = 0
+PyAverager.useblas = False
 
-#print 'Print Syst'
-#print syst
-
-#print 'Print Stat'
-#print stat
-
-print snames
-
-#initialization (optional information)
-averager.avin.initvariables()
-averager.avin.setoutputfolder('./TOutP')
-averager.avin.initeration = 0
-averager.avin.indebug = 2
-averager.avin.setsnames(snames)
-averager.avin.setfnames(fnames)
-averager.avin.setbinning(binnames, bins)
-averager.avin.inwriteoriginal = True
-averager.avin.infixstat = False
-averager.avin.incorrectstatbias = False
-averager.avin.inrescalestatsep = True
-
-#perform averaging
-dataAv,statAv,systAv = averager.average(data,stat,syst)
+# Perform averaging
+PyAverager.average(data,stat,syst,snames,fnames,bins,binnames)
